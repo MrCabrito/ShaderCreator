55e27e722bb468542d7a91220e17b67dea568eed
Abraham Gonzalez <abraham.gocape@gmail.com>
2024-06-21 14:20:55 -0600

    Updated mel commands
    Get Attr Shader: List all the attribute of the shader and shading group
    Create Texture File: Create the nodes of a texture file with the path
    Create 2D Placement: Create node for the 2d placements and connects to the texture.
    Connect Atributes: Do the connections between two attributes
    Create Bump: Create the bump node and connects the texture file
    Create Displacement: Create the displacement shader and connect to the texture file
    Dialog Window: Open a file browser from maya to select the textures.

--- utilities/mel_helper.py
+++ utilities/mel_helper.py
@@ -17,3 +17,36 @@ def assign_shader(obj_list: list[str], SG: str) -> None:
 
 def selection_shapes_meshes() -> list[str]:
     return cmds.ls(selection = True, dag=True, type="mesh", noIntermediate=True)
+
+def get_attributes_shaders(shader: str, sg:str)-> list[str]:
+    shader_attr = cmds.listAttr(shader)
+    shader_attr.extend(cmds.listAttr(sg))
+    return shader_attr
+
+def create_texture_file(shader_name:str, attr_type:str, file_path:str|None = None) -> str:
+    file_node = cmds.shadingNode('file', name = '{0}_{1}'.format(shader_name, attr_type), asTexture=True)
+    if file_path:
+        cmds.setAttr('{0}.fileTextureName'.format(file_node), file_path, type='string')
+    create_2d_placement(file_node)
+    return file_node
+
+def create_2d_placement(texture_node:str) -> None:
+    placement_node = cmds.shadingNode("place2dTexture", asTexture=True, name='place2d_{0}'.format(texture_node))
+    cmds.defaultNavigation(connectToExisting=True, source=placement_node, destination=texture_node)
+
+def connect_attributes(out_obj:str, out_attr:str, in_obj:str, in_attr:str) -> None:
+    cmds.connectAttr('{0}.{1}'.format(out_obj, out_attr), '{0}.{1}'.format(in_obj, in_attr))
+
+def create_bump(shader_name:str, texture_node:str) -> str:
+    bump_node = cmds.shadingNode('bump2d', name = '{0}_bump2d'.format(shader_name), asUtility=True)
+    connect_attributes(texture_node, 'outColorR', bump_node, 'bumpValue')
+    return bump_node
+
+def create_displacement(shader_name:str, texture_node:str) -> str:
+    displacement_node = cmds.shadingNode('displacementShader', name = '{0}_dispShd'.format(shader_name), asShader=True)
+    connect_attributes(texture_node, 'outColorR', displacement_node, 'displacement')
+    return displacement_node
+
+def dialog_window() -> list:
+    imageFilter = "Images Files (*.jpg *.jpeg *.tif *.png *.tga *.exr)"
+    return cmds.fileDialog2(fileFilter=imageFilter, dialogStyle=2, okc='Ok', fm=1)
