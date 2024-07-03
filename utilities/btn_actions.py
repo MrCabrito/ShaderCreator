c57a000fb58a678f0e782f02a41575de85d7c537
Abraham Gonzalez <abraham.gocape@gmail.com>
2024-07-02 18:24:11 -0600

    Update btn_actions.py
    Updated functions to run the sanity checks before it generates the new shader, also some function were reduce to do what the name says only

--- utilities/btn_actions.py
+++ utilities/btn_actions.py
@@ -1,21 +1,27 @@
+from __future__ import annotations
 import re
 
-def run_assign_shader(shader_name: str, shader_type: str) -> None:
-    """
-     Create and run a shader. This is a wrapper around mel_helper. create_shader and assign_shader.
-     
-     @param shader_name - Name of the shader to create.
-     @param shader_type - Type of the shader. Should be one of the constants defined in this module.
-     
-     @return Material and Shader object that was created and assigned to the meshes in the selection_shapes_mesh
-    """
-    from .mel_helper import create_shader, assign_shader, selection_shapes_meshes
-    meshes_list = selection_shapes_meshes()
-    material, sg = create_shader(shader_name, shader_type)
-    assign_shader(meshes_list, sg)
-    return material, sg
+def run_create(shader_name: str, shader_type: str, assign: bool, textures: dict) -> str|None:
+    from .mel_helper import selection_shapes_meshes
+    from .sanity_checks import main_sanity_checks
+    meshes_list = list()
+    if assign:
+        meshes_list = selection_shapes_meshes()
+    sanity_errors = main_sanity_checks(shader_name, meshes_list)
+    if sanity_errors:
+        return sanity_errors
+    material, sg = run_create_shader(shader_name, shader_type)
+    if meshes_list:
+        run_assign_shader(sg, meshes_list)
+    if textures:
+        run_connect_textures(material, textures, sg)
+    return None
 
-def run_create_shader(shader_name: str, shader_type: str) -> None:
+def run_assign_shader(sg: str, meshes_list: list[str]) -> None:
+    from .mel_helper import assign_shader
+    assign_shader(meshes_list, sg)
+
+def run_create_shader(shader_name: str, shader_type: str) -> tuple:
     """
      Create a shader and return material and shaders. This is a wrapper around mel_helper.
      
@@ -28,13 +34,15 @@ def run_create_shader(shader_name: str, shader_type: str) -> None:
     material, sg = create_shader(shader_name, shader_type)
     return material, sg
 
-def run_connect_textures(shader: str, textures: dict, sg:str):
+def run_connect_textures(shader: str, textures: dict, sg:str) -> None:
     """
      Connect textures to a subsurface.
      
      @param shader - name of the shader to connect to.
      @param textures - dictionary of attributes to be connected to the subsurface.
      @param sg - name of the subsurface to connect to. If None the shader is connected to the main
+
+     @return None
     """
     from .mel_helper import (get_attributes_shaders, 
                              create_texture_file, 
