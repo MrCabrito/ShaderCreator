c216270b7d5a8d061a872ef28989ce776b3942b9
Abraham Gonzalez <abraham.gocape@gmail.com>
2024-07-04 01:10:54 -0600

    Created documentation for the new feature

--- utilities/path_helper.py
+++ utilities/path_helper.py
@@ -29,15 +29,29 @@ def path_udim(file_path: str) -> tuple:
     return file_path, udim
 
 def path_look_relatives(file_path: str) -> dict:
+    """
+     Look for relatives in a path and return a dictionary. This is a helper function for path_look ()
+     
+     @param file_path - path to look for relatives
+     
+     @return dict with relative paths as keys and lists of file
+    """
+    # Split the path and file name of the given path
     path, file = file_path.rsplit("/", 1)
+    # Look if the giving file has an specific map type
     map_type = re.search('([Dd]iffuse)|([Ss]pecular)|([Rr]oughness)|([Tt]ransmission)|([Ss]ssColor)|([Ss]ss)|([Bb]ump)|([Dd]isplacement)', file)
     if map_type:
+        # Look for all the file in the same path of the file given
         files_found = [file_found for file_found in os.listdir(path) if os.path.isfile(os.path.join(path,file_found))]
+        
         re_pattern = r'{}(([Dd]iffuse)|([Ss]pecular)|([Rr]oughness)|([Tt]ransmission)|([Ss]ssColor)|([Ss]ss)|([Bb]ump)|([Dd]isplacement))'.format(file.split(map_type.group())[0])
         re_compile = re.compile(re_pattern)
         files_relative = dict()
+
+        # Loop through the files looking for a pattern similar of the given file
         for file_match in files_found:
             re_match = re.search(re_compile, file_match)
             if re_match:
                 files_relative[(re_match.group().replace(file.split(map_type.group())[0], ''))]='{0}/{1}'.format(path,file_match)
+        # Returns a dictionary of the map type and path founded
         return files_relative
--- utilities/sanity_checks.py
+++ utilities/sanity_checks.py
@@ -2,6 +2,15 @@ from __future__ import annotations
 import re
 
 def main_sanity_checks(name:str, objs_list: list[str], textures: dict) -> str:
+    """
+     Performs sanity checks on the name surface and textures. This is a helper function to allow other modules to add a few things to the output
+     
+     @param name - The name of the object
+     @param objs_list - A list of object names to check
+     @param textures - A dictionary of path to texture files that should be checked
+     
+     @return Error message or empty string if everything is OK
+    """
     from .mel_helper import surface_check
     error_message = ""
     surface_error = list()
@@ -10,17 +19,22 @@ def main_sanity_checks(name:str, objs_list: list[str], textures: dict) -> str:
     # Check if the surface is valid.
     if objs_list:
         surface_error = surface_check(objs_list)
+    # Check if the path is empty or not.
     if textures:
         empty_textures = path_is_empty_check(textures)
         path_not_found = path_exists_check(textures)
 
     # Add text if there is some errors found, depending where it didn't pass the sanity check.
+    # If name_error is true the name is not a name error message.
     if name_error:
         error_message = '<font color="orange", size="25"><b>Name Warning:</b></font><br />There are some special characters in the name:<br /><font color="red", size="4"><b>{}</b></font>, remove them.<br />'.format(name)
+    # If surface_error is set to true the error message is shown in the surface_error message.
     if surface_error:
         error_message = '{0}<br /><font color="orange", size="25"><b>Objects Selected Warning:</b></font><br />There are some objects selected that can\'t be assign a shader, select only MESHES or a NURB SURFACE.<br /><font color="red", size="4"><b>{1}</b></font><br />'.format(error_message,"<br />".join(surface_error))
+    # If empty_textures is set to true the error message is shown in the error message.
     if empty_textures:
         error_message = '{0}<br /><font color="orange", size="25"><b>Empty Textures Warning:</b></font><br />There are some empty texture path, select a texture or uncheck them.<br /><font color="red", size="4"><b>{1}</b></font><br />'.format(error_message,"<br />".join(empty_textures))
+    # If path_not_found is true the path is not found in your computer.
     if path_not_found:
         error_message = '{0}<br /><font color="orange", size="25"><b>Path not Found Warning:</b></font><br />There are some files that doesn\'t exist in your computer, select an existing one.<br /><font color="red", size="4"><b>{1}</b></font><br />'.format(error_message,"<br />".join(path_not_found))
 
@@ -60,6 +74,5 @@ def path_exists_check(textures: dict) -> list[str]:
      @return list of paths that don't exist in file
     """
     import os
-    # path_not_found_dict = {map_type: texture_path for map_type,texture_path in textures.items() if texture_path and not os.path.isfile(texture_path)}
     path_not_found = ['{0}: {1}'.format(map_type, texture_path) for map_type,texture_path in textures.items() if texture_path and not os.path.isfile(texture_path)]
     return path_not_found
